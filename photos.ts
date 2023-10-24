8baccd9ad14ebaeeb1d40df0d6f504f92cc95fd1
Hyeokjin Doo <hdoo@c1r2s6.42seoul.kr>
2023-10-24 23:13:04 +0900

    delete modal

--- photos.ts
+++ photos.ts
@@ -1,5 +1,7 @@
 import { StaticImageData } from "next/image";
-import yeongo from "@/public/picture/20220826_yeongo/main.jpg";
+import yeongo_main from "@/public/picture/20220826_yeongo/main.jpg";
+import yeongo_sub1 from "@/public/picture/20220826_yeongo/sub1.jpg";
+import yeongo_sub2 from "@/public/picture/20220826_yeongo/sub2.jpg";
 export type Photo = {
   id: string;
   name: string;
@@ -14,7 +16,14 @@ const photos: Photo[] = [
     name: "yeongo",
     href: "https://twitter.com/kvncnls/status/1471832344986324998",
     username: "@kvncnls",
-    imageSrc: yeongo,
+    imageSrc: yeongo_main,
+  },
+  {
+    id: "2",
+    name: "yeongo",
+    href: "https://twitter.com/kvncnls/status/1471832344986324998",
+    username: "@kvncnls",
+    imageSrc: yeongo_sub1,
   },
 ];
 
